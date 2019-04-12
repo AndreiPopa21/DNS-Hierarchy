565b17b7a1ff624ee2eef73a3132ab6adcb050ab
AndreiPopa21 <andrei.popa2199@gmail.com>
2019-04-12 04:17:35 +0300

    Implemented structs definitions

--- main.c
+++ main.c
@@ -8,5 +8,20 @@
 
 int main(){
     fprintf(stdout,"Created exe\n");
+    test_generic_list();
     return 0;
+}
+
+void test_generic_list(){
+    List my_list;
+    users_node_t head_users_node;
+    users_node_t tail_users_node;
+
+    my_list.head = &head_users_node;
+    my_list.tail = &tail_users_node; 
+
+    head_users_node.user_index = 5;
+    tail_users_node.user_index = 18;
+
+    fprintf(stdout,"Head: %d\nTail:\n",((users_node_t*)my_list.head)->user_index);
 }
--- structs.h
+++ structs.h
@@ -8,4 +8,37 @@
 #include "utils.h"
 #include "tasks.h"
 
+typedef struct List{
+    void* head;
+    void* tail;
+    int nodes_count;
+}List;
+
+typedef struct address_node_t{
+    char* name;
+    struct address_node_t* next;
+    struct address_node_t* prev;
+}address_node_t;
+
+typedef struct dns_server_t{
+    struct dns_server_t* parent;
+    struct List* addresses;
+    struct List* children;
+    struct List* users;
+    int isFault;
+}dns_server_t;
+
+typedef struct users_node_t{
+    int user_index;
+    struct dns_server_t* dns_server;
+    struct users_node_t* next;
+    struct users_node_t* prev;
+}users_node_t;
+
+typedef struct dns_node_t{
+    struct dns_servet_t* dns_server;
+    struct dns_node_t* next;
+    struct dns_node_t* prev;
+}dns_node_t;
+
 #endif
