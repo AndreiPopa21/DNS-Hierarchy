c177a3fbb1df347bad771a4d645e6bfc80b5fa9f
AndreiPopa21 <andrei.popa2199@gmail.com>
2019-04-22 23:28:49 +0300

    Format code in tasks.c

--- tasks.c
+++ tasks.c
@@ -3,14 +3,16 @@
 
 void tree_construction(Hierarchy_t** hierarchy){
     
+    int i;
+    int j;
+    int servers_count = 0;
+
     if(!(*hierarchy)){
         fprintf(stdout,"Hierarchy is NULL\n");
         return;
     }
 
-    int servers_count = 0;
     temp_dns_struct_t** temp_dns = read_from_tree_in(hierarchy,&servers_count);
-    int i;
 
     (*hierarchy)->servers_count = servers_count;
 
@@ -26,7 +28,6 @@ void tree_construction(Hierarchy_t** hierarchy){
         }
     }
     
-    int j;
     for(i = 0; i < servers_count; i++){
         int par_index = temp_dns[i]->parent_index;
         for(j = 0; j < servers_count; j++){
@@ -38,6 +39,7 @@ void tree_construction(Hierarchy_t** hierarchy){
             }
         }
     }
+
     (*hierarchy)->root->parent = NULL;
     
     for(i = 0; i<servers_count; i++){
@@ -48,9 +50,11 @@ void tree_construction(Hierarchy_t** hierarchy){
     }
     
     FILE* fh = fopen("tree.out","w+");
+    
     read_children_index_recursively(&(*hierarchy)->root,fh);
 
     fclose(fh);
+
     free_temp_dns_array(&temp_dns,servers_count);
     free(server_array);
 }
@@ -61,15 +65,29 @@ void hierarchy_initialization(Hierarchy_t** hierarchy){
         fprintf(stdout,"Passed NULL hierarchy to second task\n");
         return;
     }
+
     FILE* fh = fopen("hierarchy.out","w+");
+
+    if(!fh){
+        fprintf(stdout,"Could not open hierarchy.out\n");
+        return;
+    }
 
     dns_server_t* root = (*hierarchy)->root;
     cluster_children_addresses(NULL,&root);
     read_dns_servers_recursively(&root,fh);
+
     fclose(fh);
 }
 
 void user_queries(Hierarchy_t** hierarchy, user_list_t** users_list){
+
+    int i;
+    int users_count = 0;
+    int user_index;
+    int server_index;
+    int queries_count = 0;
+    char param[10];
 
     if(!(*hierarchy)){
         fprintf(stdout,"Passed NULL hierarchy to third task\n");
@@ -81,46 +99,46 @@ void user_queries(Hierarchy_t** hierarchy, user_list_t** users_list){
     }
 
     FILE* fh = fopen("users.in","r+");
+    FILE* q_fh = fopen("queries.out","w+");
 
     if(!fh){
         fprintf(stdout,"Could not open users.in file\n");
         return;
     }
+    
+    if(!q_fh){
+        fprintf(stdout,"Could not open queries.out file\n");
+        return;
+    }
 
-    int users_count = 0;
     fscanf(fh,"%d",&users_count);
 
-    int i;
-    int user_index;
-    int server_index;
+    if(!q_fh){
+        fprintf(stdout,"Could not open queries.out file\n");
+        return;
+    }
+
     for(i=0;i<users_count;i++){
         fscanf(fh,"%d",&user_index);
         fscanf(fh,"%d",&server_index);
         push_user_node(hierarchy,users_list,user_index,server_index);
         
     }
+
     fclose(fh);
 
     user_node_t** found_user = (user_node_t**)calloc(1,sizeof(user_node_t*));
     dns_server_t** found_server = (dns_server_t**)calloc(1,sizeof(dns_server_t*));
     
     fh = fopen("queries.in","r+");
-    FILE* q_fh = fopen("queries.out","w+");
 
     if(!fh){
         fprintf(stdout,"Could not open queries.in file\n");
         return;
     }
 
-    if(!q_fh){
-        fprintf(stdout,"Could not open queries.out file\n");
-        return;
-    }
-    
-    int queries_count = 0;
     fscanf(fh,"%d",&queries_count);
     
-    char param[10];
     char** looked_addresses = (char**)calloc(queries_count,sizeof(char*));
     
     for(i = 0;i<queries_count;i++){
@@ -141,16 +159,14 @@ void user_queries(Hierarchy_t** hierarchy, user_list_t** users_list){
                 dns_server_t* user_server = (*found_user)->server;
                 traverse_bottom_up_for_address(&user_server,looked_addresses[i],q_fh);
             }
-
-        }else{
+        } else {
             if(strcmp(param,"f")==0){
                 (*found_server)=NULL;
                 fscanf(fh,"%d",&server_index);
                 int result = get_server(&((*hierarchy)->root),found_server,server_index);
                 if(result == 1){
                     pass_faulty_server_users(found_server,users_list);
-                }
-                
+                }  
             }
         }
     }
@@ -160,11 +176,11 @@ void user_queries(Hierarchy_t** hierarchy, user_list_t** users_list){
             free(looked_addresses[i]);
         }
     }
-    free(looked_addresses);
-
+    
     fclose(fh);
     fclose(q_fh);
 
+    free(looked_addresses);
     free(found_user);
     free(found_server);
 }
